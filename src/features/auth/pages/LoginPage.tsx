4ad2e24a6805eacc76c10fe984784fe034d9e533
dodeteddydev <dodeteddydev@gmail.com>
2026-08-10 22:06:16 +0800

    feat(auth): slicing UI not fix yet

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,10 +1,40 @@
+import { Input } from "@/components/Input/Input";
 import styles from "./LoginPage.module.css";
+import { Button } from "@/components/Button/Button";
 
 export const LoginPage = () => {
   return (
-    <section className={styles.wrapper}>
-      <div className={styles.leftSection}>LEFT</div>
-      <div className={styles.rightSection}>RIGHT</div>
-    </section>
+    <main className={styles.wrapper}>
+      <section>
+        <p className={styles.appTitle}>Mandalika Store Sales and Stock</p>
+        <p className={styles.loginTitle}>Log in to your account.</p>
+        <p className={styles.description}>
+          Enter your username and password to log in.
+        </p>
+      </section>
+
+      <section>
+        <form className={styles.wrapperForm}>
+          <Input
+            id="username"
+            label="Username"
+            placeholder="e.g. johndoe"
+            type="text"
+            error="Username is required"
+          />
+          <Input
+            id="password"
+            label="Password"
+            placeholder="e.g. 123456"
+            type="password"
+            error="Password is required"
+          />
+
+          <div className={styles.buttonWrapper}>
+            <Button>Login</Button>
+          </div>
+        </form>
+      </section>
+    </main>
   );
 };
